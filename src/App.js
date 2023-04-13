43b360199fe31b0882eb2d7a33c92b93c72dc73b
zhengyangca <billyoung119@gmail.com>
2023-04-12 21:45:53 -0600

    add gc indicators

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ class App extends Component {
     this.state = {
       corra: [],
       cpi: [],
+      ca_indicators: [],
+      ab_indicators: [],
+      on_indicators: [],
+      bc_indicators: [],
     };
   }
   componentDidMount = () => {
@@ -17,6 +21,18 @@ class App extends Component {
     fetch(baseurl + "/cpi?recent=23")
       .then((response) => response.json())
       .then((cpi) => this.setState({ cpi }));
+    fetch(baseurl + "/gc-econ?geo_code=0")
+      .then((response) => response.json())
+      .then((ca_indicators) => this.setState({ ca_indicators }));
+    fetch(baseurl + "/gc-econ?geo_code=9")
+      .then((response) => response.json())
+      .then((ab_indicators) => this.setState({ ab_indicators }));
+    fetch(baseurl + "/gc-econ?geo_code=6")
+      .then((response) => response.json())
+      .then((on_indicators) => this.setState({ on_indicators }));
+    fetch(baseurl + "/gc-econ?geo_code=10")
+      .then((response) => response.json())
+      .then((bc_indicators) => this.setState({ bc_indicators }));
   };
 
   render() {
@@ -24,6 +40,7 @@ class App extends Component {
     const rows = corra.map((row, index) => {
       return (
         <tr key={index}>
+          <hr />
           <td>{row.date}</td>
           <td>
             <div style={{ padding: "10px" }} />
@@ -44,6 +61,58 @@ class App extends Component {
         </tr>
       );
     });
+    const { ab_indicators } = this.state;
+    const abSliced = ab_indicators.slice(0, 2);
+    const abRows = abSliced.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.titleEn}</td>
+          <td>
+            <div style={{ padding: "10px" }} />
+          </td>
+          <td>{row.valueEn}</td>
+        </tr>
+      );
+    });
+    const { ca_indicators } = this.state;
+    const caSliced = ca_indicators.slice(0, 2);
+    const caRows = caSliced.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.titleEn}</td>
+          <td>
+            <div style={{ padding: "10px" }} />
+          </td>
+          <td>{row.valueEn}</td>
+        </tr>
+      );
+    });
+    const { on_indicators } = this.state;
+    const onSliced = on_indicators.slice(0, 2);
+    const onRows = onSliced.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.titleEn}</td>
+          <td>
+            <div style={{ padding: "10px" }} />
+          </td>
+          <td>{row.valueEn}</td>
+        </tr>
+      );
+    });
+    const { bc_indicators } = this.state;
+    const bcSliced = bc_indicators.slice(0, 2);
+    const bcRows = bcSliced.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.titleEn}</td>
+          <td>
+            <div style={{ padding: "10px" }} />
+          </td>
+          <td>{row.valueEn}</td>
+        </tr>
+      );
+    });
 
     return (
       <div style={{ display: "flex" }}>
@@ -60,21 +129,58 @@ class App extends Component {
           </table>
         </div>
         <div style={{ flex: 1, backgroundColor: "grey" }}>
-          {/* Content for the second column */}
+          <table>
+            <thead>
+              <tr>
+                <th>CA</th>
+                <th></th>
+                <th>Indicator</th>
+              </tr>
+            </thead>
+            <tbody>{caRows}</tbody>
+          </table>
+          <table>
+            <thead>
+              <tr>
+                <th>AB</th>
+                <th></th>
+                <th>Indicator</th>
+              </tr>
+            </thead>
+            <tbody>{abRows}</tbody>
+          </table>
+          <table>
+            <thead>
+              <tr>
+                <th>ON</th>
+                <th></th>
+                <th>Indicator</th>
+              </tr>
+            </thead>
+            <tbody>{onRows}</tbody>
+          </table>
+          <table>
+            <thead>
+              <tr>
+                <th>BC</th>
+                <th></th>
+                <th>Indicator</th>
+              </tr>
+            </thead>
+            <tbody>{bcRows}</tbody>
+          </table>
         </div>
         <div style={{ flex: 1, backgroundColor: "" }}>
-          <div style={{ flex: 1, backgroundColor: "" }}>
-            <table>
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th></th>
-                  <th>CPI</th>
-                </tr>
-              </thead>
-              <tbody>{cpiRows}</tbody>
-            </table>
-          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th></th>
+                <th>CPI</th>
+              </tr>
+            </thead>
+            <tbody>{cpiRows}</tbody>
+          </table>
         </div>
         <div style={{ flex: 1, backgroundColor: "green" }}>
           {/* Content for the third column */}
